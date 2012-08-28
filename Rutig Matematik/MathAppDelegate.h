260efe081decc3cb421313a3107feeb6d5c74b30
Richard Henriksson <richen@student.chalmers.se>
2012-08-28 21:43:44 +0200

    Initial Commit

---
+++ Rutig Matematik/MathAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MathAppDelegate.h
+//  Rutig Matematik
+//
+//  Created by Richard Henriksson on 2012-08-28.
+//  Copyright (c) 2012 Richard Henriksson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MathAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rutig Matematik/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rutig Matematik
+//
+//  Created by Richard Henriksson on 2012-08-28.
+//  Copyright (c) 2012 Richard Henriksson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MathAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MathAppDelegate class]));
+    }
+}
